fc20d5e668fa9a1cf61e1f9a61c4127fffee5039
Ujjwal Parashar <whomtalkto@gmail.com>
2025-01-12 00:48:29 +0530

    updated package.json

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000" : "/api",
+  baseURL:"https://tech-redefined-http-server.onrender.com/",
   withCredentials: true,
 });
